14953d885630ae298ed6958f0f097a00ff1f27d2
maubolwork-projects <maubol.hdi@gmail.com>
2026-07-17 15:18:22 -0600

    Add schema validation functions for data alignment
    This script contains functions to align table structures between source data and existing RAW database tables, facilitating the loading process without discrepancies.

---
+++ Scripts/Pipeline1.1/Schema_validation/schema_validation.py
@@ -0,0 +1,81 @@
+# schema_validation.py
+
+""" Esta librería contiene funciones que ayudan a alinear las estructuras de las tablas entre las fuentes
+    y las existentes en la base RAW, para en un proceso posterior poder cargarlas a RAW sin desfases.
+"""
+
+#====================================
+# 1. Funciones Privadas (Uso Interno)
+#====================================
+from sqlalchemy import create_engine, text
+import pandas as pd
+
+def _normalize_schema_headers(df):
+    # Esta función recorta los headers cuando son demasiado largos y toma solo las dos cadenas unidas
+    # por ">", ejemplo: material>cemento
+    df_copy = df.copy()
+    # EXPRESIÓN REGULAR: Sin espacios internos y con soporte para espacios opcionales alrededor del '>'
+    patron = r'([^>]+(?:\s*>\s*)[^>]+)$'
+    
+    series_columns = df_copy.columns.to_series().reset_index(drop=True)
+    # Extraemos las últimas dos secciones y rellenamos con la original si no hay cambios
+    cutting_headers = series_columns.astype(str).str.extract(patron, expand=False).fillna(series_columns)
+    df_copy.columns = cutting_headers.str.strip().values
+    return df_copy
+
+def _extra_columns(df_source, df_raw):
+    #Compara el nombre de las columnas de la fuente contra las registradas en la base RAW.
+    #Primero corta la cadena hasta las últimas dos cadenas que construyen el header despues,
+    #las agrupa en conjuntos y se obtienen las columnas extra en la nueva fuente.
+    df_source = _normalize_schema_headers(df_source)
+    name_headers_source = set(df_source.columns)
+    df_raw = _normalize_schema_headers(df_raw)
+    name_headers_raw = set(df_raw.columns)
+    extra_columns_excel = list(name_headers_source - name_headers_raw)
+    extra_columns_raw = list(name_headers_raw - name_headers_source)
+    return extra_columns_excel, extra_columns_raw
+
+def _recover_original_headers(df, pieces_list):
+    #Devuelve una lista con los nombres originales de las columnas  que coinciden con la lista de fragmentos proporcionada.
+    
+    original_indices = [i for i, col in enumerate(df.columns) if any(col.endswith(f) for f in pieces_list)]
+    original_headers = list(df.columns[original_indices])
+    return original_headers
+
+def _create_new_raw_columns(headers, table_name, engine):
+    #Esta función agrega las columnas nuevas extraidas del DF proveniente de Excel a la base RAW
+    with engine.begin() as conexion:
+        for col in headers:
+            querry_sql = text(f'ALTER TABLE "{table_name}" ADD COLUMN IF NOT EXISTS "{col}" VARCHAR;')
+
+            conexion.execute(querry_sql)
+    return None
+
+def _align_dataframe_schema(df, raw_headers):
+    #Esta función agrega las columnas que tenga faltantes con respecto a RAW y las agrega para cuadrar la carga
+    df_copy = df.copy()
+    for col in raw_headers:
+        if col not in df_copy.columns:
+            df_copy[col] = pd.Series(pd.NA, index=df_copy.index, dtype="string")
+    return df_copy
+
+#====================================
+# 2. Función Pública (Interfaz)
+#====================================
+
+def schema_validation(df_source, df_raw, table_name, engine):
+    #Esta función detecta si hay columnas nuevas en el df proveniente de Excel, si las hay las agrega
+    #  a la base RAW, importa las columnas faltantes que existen en RAW y devuelve un DF enriquecido
+    # para su posterior carga a RAW
+    df_enriched = df_source.copy()
+    extra_columns, extra_raw = _extra_columns(df_source, df_raw)
+    if extra_columns:
+        original_headers = _recover_original_headers(df_source, extra_columns)
+        _create_new_raw_columns(original_headers, table_name, engine)
+    if extra_raw:
+        original_raw = _recover_original_headers(df_raw, extra_raw)
+        df_enriched = _align_dataframe_schema(df_source, original_raw)
+        
+    return df_enriched
+
+__all__ = ['schema_validation']
